c18a8327d82a5a44d0e23157b8b49b496eef2f93
ghoshshreya <shrghosh08@gmail.com>
2023-01-22 13:34:28 +0530

    1 file modified

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -137,3 +137,61 @@ var list = new SinglyLinkedList();
 
 console.log('Inserting New Node at Position', 0, list.insert(1, 'New Value 4'));
 list.traverse();
+
+function generateListFromArray(arr) {
+  let ll = {
+    val: null,
+    next: null,
+  };
+  let tail = null;
+  for (let x of arr) {
+    if (ll.val === null) {
+      ll.val = x;
+      tail = ll;
+    } else {
+      let newNode = {
+        val: x,
+        next: null,
+      };
+      tail.next = newNode;
+      tail = newNode;
+    }
+  }
+  return ll;
+}
+
+let l1 = generateListFromArray([6, 4, 5, 0, 4, 4, 9, 4, 1]);
+let l2 = generateListFromArray([3, 8, 8, 0, 3, 0, 1, 4, 8]);
+
+var addTwoNumbers = function (l1, l2) {
+  let carry = 0;
+  let node = new Node(null);
+  let tail = node;
+  while (l1?.val >= 0 || l2?.val >= 0) {
+    let sum = (l1 && l1.val ? l1.val : 0) + (l2 && l2.val ? l2.val : 0) + carry;
+    if (sum >= 10) {
+      sum = sum % 10;
+      carry = 1;
+    } else carry = 0;
+    if (node.val === null) {
+      node.val = sum;
+      tail = node;
+    } else {
+      let newNode = new Node(sum);
+      tail.next = newNode;
+      tail = newNode;
+    }
+    console.log(l1);
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
+  }
+
+  if (carry) {
+    let newNode = new Node(carry);
+    tail.next = newNode;
+    tail = newNode;
+  }
+  return node;
+};
+
+console.log(addTwoNumbers(l1, l2));
