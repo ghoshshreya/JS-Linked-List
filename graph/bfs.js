b54d5099e0cf8f8ae0ecd518d158cdb101d543fb
ghoshshreya <shrghosh08@gmail.com>
2023-03-05 23:22:11 +0530

    1 file modified

--- graph/bfs.js
+++ graph/bfs.js
@@ -39,6 +39,27 @@ class Graph {
     return explorationSet;
   }
 
+  isCyclicBfs(sourceNode) {
+    console.log(this.adjList);
+    let visitedNode = {};
+    for (let x = 0; x < this.V; x++) {
+      visitedNode[x] = false;
+    }
+    let queue = [];
+    queue.push([sourceNode, -1]);
+    visitedNode[sourceNode] = 1;
+    while (queue.length) {
+      let elem = queue.shift();
+      visitedNode[elem] = true;
+      console.log(elem);
+      this.adjList[elem[0]].forEach((x) => {
+        if (visitedNode[x]) return true;
+        // queue.push([x, elem]);
+      });
+    }
+    return false;
+  }
+
   dfs(sourceNode) {
     let stack = [];
     var visited = new Set();
@@ -58,3 +79,4 @@ graph1.addEdge(3, 3);
 let edges = graph1.edges;
 console.log('BFS', graph1.bfs(2));
 console.log('DFS', graph1.dfs(2));
+console.log('isCyclicBfs', graph1.isCyclicBfs(1));
