de477c36066911e3d287bd329cc65bfc0547eab8
ghoshshreya <shrghosh08@gmail.com>
2023-03-05 12:22:39 +0530

    2 files modified

--- graph/dfs.js
+++ graph/dfs.js
@@ -2,15 +2,30 @@ class Graph {
   constructor(v) {
     this.V = v;
     this.adjList = new Array(v);
-    for (let x of this.adjList) {
-      x = [];
+    for (let x = 0; x < this.adjList.length; x++) {
+      this.adjList[x] = [];
     }
-
-    console.log(this.adjList);
   }
 
-  addEdge(u, v) {
+  //undirected
+  addEdge_undirected(u, v) {
     this.adjList[u].push(v);
+    this.adjList[v].push(u);
+  }
+
+  dfs(source) {
+    let visistedQueue = new Set();
+    let stack = [];
+    stack.push(source);
+    while (stack.length) {
+      let elem = stack.pop();
+      visistedQueue.add(elem);
+      // this.adjList[elem].forEach((x) => {
+      //   if (!visistedQueue.has(x)) {
+      //     stack.push(x);
+      //   }
+      // });
+    }
   }
 
   get edges() {
@@ -18,10 +33,11 @@ class Graph {
   }
 }
 
-let graph = new Graph();
-graph.addEdge(1, 2);
-graph.addEdge(2, 3);
-graph.addEdge(2, 6);
-graph.addEdge(3, 4);
+let graph = new Graph(4);
+graph.addEdge_undirected(0, 1);
+graph.addEdge_undirected(1, 2);
+graph.addEdge_undirected(1, 3);
+graph.addEdge_undirected(2, 3);
 
 console.log(graph.edges);
+graph.dfs(0);
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import './BinarySearchTree.js';
 import './BST-Problem.js';
 import './queue/queue.js';
 import './graph/bfs.js';
-import './graph/cycleDetection.js';
 import './graph/dfs.js';
 
 // Write Javascript code!
