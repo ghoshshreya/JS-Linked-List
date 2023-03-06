2cb5d2d5d807aab3b06ec7f8fdc58d15e49b4df1
ghoshshreya <shrghosh08@gmail.com>
2023-03-06 11:52:01 +0530

    1 file modified

--- tree/treeTraversal.js
+++ tree/treeTraversal.js
@@ -1,24 +1,26 @@
-// var inorderTraversal = function (root) {
-//   const inorder = [];
-//   while (true) {
-//     if (root === null) return;
-//     inorderTraversal(root.left);
-//     console.error(root.val);
-//     // inorder.push(root.val);
-//     inorderTraversal(root.right);
-//   }
-// };
+var inorderTraversal = function (root) {
+  const inorder = [];
+  let i = 0;
+  while (true && i < 100) {
+    if (root === null) return;
+    inorderTraversal(root.left);
+    console.error(root.val);
+    // inorder.push(root.val);
+    inorderTraversal(root.right);
+    i++;
+  }
+};
 
-// inorderTraversal({
-//   val: 1,
-//   left: null,
-//   right: {
-//     val: 2,
-//     left: {
-//       val: 3,
-//       left: null,
-//       right: null,
-//     },
-//     right: null,
-//   },
-// });
+inorderTraversal({
+  val: 1,
+  left: null,
+  right: {
+    val: 2,
+    left: {
+      val: 3,
+      left: null,
+      right: null,
+    },
+    right: null,
+  },
+});
